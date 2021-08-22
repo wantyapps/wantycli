b0f35693593d3cd33667e47dbc81e86357ab358b
wantyapps <56798701+wantyapps@users.noreply.github.com>
2021-08-22 21:17:53 +0300

    Add debug mode message

--- main.go
+++ main.go
@@ -32,42 +32,45 @@ func main() {
 	}
 
 	switch os.Args[1] {
-		case "api":
-			apiCmd.Parse(os.Args[2:])
-			if *apiURL != "" {
-				client := &http.Client{}
-				req, _ := http.NewRequest("GET", *apiURL, nil)
-				req.Header.Set("username", *apiUsername)
-				req.Header.Set("password", *apiPassword)
-				if *apiAction != "" {
-					req.Header.Set("action", *apiAction)
-				}
-
-				res, err := client.Do(req)
-				if err != nil {
-					if *debugMode == true {
-						log.Println(err)
-					}
-					fmt.Println("[\033[91mERROR\033[0m] Connection error.")
-					os.Exit(1)
-				}
-				defer res.Body.Close()
-				body, err := ioutil.ReadAll(res.Body)
-				if err != nil {
-					if *debugMode == true {
-						log.Println(err)
-					}
-					fmt.Println("[\033[91mERROR\033[0m] Error reading response body.")
-				}
-
-				fmt.Println(string(body))
-			} else {
-				usage()
-				os.Exit(1)
+	case "api":
+		apiCmd.Parse(os.Args[2:])
+		if *debugMode == true {
+			fmt.Println("Debug mode: activated")
+		}
+		if *apiURL != "" {
+			client := &http.Client{}
+			req, _ := http.NewRequest("GET", *apiURL, nil)
+			req.Header.Set("username", *apiUsername)
+			req.Header.Set("password", *apiPassword)
+			if *apiAction != "" {
+				req.Header.Set("action", *apiAction)
 			}
 
-		default:
+			res, err := client.Do(req)
+			if err != nil {
+				if *debugMode == true {
+					log.Println(err)
+				}
+				fmt.Println("[\033[91mERROR\033[0m] Connection error.")
+				os.Exit(1)
+			}
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				if *debugMode == true {
+					log.Println(err)
+				}
+				fmt.Println("[\033[91mERROR\033[0m] Error reading response body.")
+			}
+
+			fmt.Println(string(body))
+		} else {
 			usage()
 			os.Exit(1)
+		}
+
+	default:
+		usage()
+		os.Exit(1)
 	}
 }
